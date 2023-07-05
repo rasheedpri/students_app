4e01f34290e75b70a792d6660bd1dddd30159c2e
Mufeednpd <90769002+Mufeednpd@users.noreply.github.com>
2023-07-05 18:22:47 +0530

    Update sg.tf

--- iac/sg.tf
+++ iac/sg.tf
@@ -37,3 +37,9 @@ resource "aws_security_group" "inbound_rule" {
 
   }
 }
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  security_group_id    = aws_security_group.inbound_rule.id
+  network_interface_id = aws_instance.instance.primary_network_interface_id
+}
+
+
