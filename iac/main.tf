20d30626d988391c1a2af0ca94ce77e82d96b8ff
Mufeednpd <90769002+Mufeednpd@users.noreply.github.com>
2023-07-09 07:55:47 +0530

    Update main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -23,8 +23,8 @@ resource "aws_eip" "ec2" {
 # ec2 instance 
 
 resource "aws_instance" "ec2" {
-  ami           = "ami-053b0d53c279acc90" # us-east-1a
-  instance_type = "t2.medium"
+  ami           = "ami-053b0d53c279acc90" # us-east-1
+  instance_type = "t2.micro"
   key_name = "test"
 
   network_interface {
