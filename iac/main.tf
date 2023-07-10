d8ab89e92589ae08aa2f5318666ace959aeeba11
Mufeednpd <90769002+Mufeednpd@users.noreply.github.com>
2023-07-10 07:18:57 +0530

    Update main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -12,20 +12,13 @@ resource "aws_network_interface" "ec2" {
 }
 
 
-# elastic ip
-
-resource "aws_eip" "ec2" {
-  domain                    = "vpc"
-  network_interface         = aws_network_interface.ec2.id
-  associate_with_private_ip = cidrhost("${var.cidr_block}", 6)
-}
-
 # ec2 instance 
 
 resource "aws_instance" "ec2" {
   ami           = "ami-053b0d53c279acc90" # us-east-1
   instance_type = "t2.micro"
   key_name = "test"
+  associate_public_ip_address = true
 
   network_interface {
     network_interface_id = aws_network_interface.ec2.id
