14ec1fe75891032573ddc71023fac7d7ebbc9540
Mufeednpd <90769002+Mufeednpd@users.noreply.github.com>
2023-07-10 12:08:32 +0530

    Update network.tf

--- iac/network.tf
+++ iac/network.tf
@@ -14,8 +14,7 @@ resource "aws_subnet" "subnet" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = cidrsubnet(var.cidr_block, 2, 0)
   availability_zone = "${var.region}a"
-  
-
+  map_public_ip_on_launch = true
 }
 
 
